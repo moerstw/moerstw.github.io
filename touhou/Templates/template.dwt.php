0ae9cfe49b3ab689433c8857e8e9d36b08e10cc8
moerstw <moerstw@gmail.com>
2014-07-27 14:38:48 +0800

    touhou page updata

---
+++ touhou/Templates/template.dwt.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- TemplateBeginEditable name="includecssjs" -->  <!-- TemplateEndEditable -->
+
+<!-- #BeginEditable "Javascript" --> 
+
+<!-- #EndEditable -->
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>東方考古日誌</title>
+<!-- TemplateEndEditable -->
+<link href="../css/style.css" rel="stylesheet" type="text/css" />
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+
+</head>
+
+<body>
+<div id="container">
+
+<div id="header2">
+</div>
+
+<ul id="menu">
+	<li id="me-ind"><a href="../index.php"></a></li>
+    <li id="me-abo"><a href="../about.php"></a></li>
+    <li id="me-cha"><a href="../character.php"></a></li>
+    <li id="me-ter"><a href="../term.php"></a></li>
+    <li id="me-gen"><a href="../gensoukyou.php"></a></li>
+    <li id="me-lin"><a href="../link.php"></a></li>
+</ul>
+<div id=content2">
+<div id="leftbar"><!-- TemplateBeginEditable name="EditRegion3" -->EditRegion3<!-- TemplateEndEditable --></div>
+<div id="rightcontent"><!-- TemplateBeginEditable name="EditRegion4" -->EditRegion4<!-- TemplateEndEditable --></div>
+</div>
+
+<div id="footer">
+    建議最佳瀏覽 IE6.0 以上版本瀏覽器 1024x768像素以上的解析度<br />
+    101網頁設計與管理|Design By moerstw|E-mail: moerstw@gmail.com<br />
+    CopyRight&copy;2013 moerstw All rights Reserved.
+</div>
+
+
+
+</div><!-- container-->
+</body>
+</html>
